6e08a1cfdd48cdb5268f396877ce900f42e969e8
PikaKight <tmmarcus73@gmail.com>
2021-01-21 16:59:40 -0500

    Fixed the mapview. Error was caused by uppercasing the L on lat

--- map.js
+++ map.js
@@ -1,8 +1,44 @@
-let map;
+let map, infoWindow;
 
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
-        center: {Lat: 43.0056, lng: -81.2760},
-        zoom: 15,
+        center: { lat: 43.0056, lng: -81.2760 },
+        zoom: 18,
     });
+    infoWindow = new google.maps.InfoWindow();
+    const locationButton = document.createElement("button");
+    locationButton.textContent = "Pan to Current Location";
+    locationButton.classList.add("custom-map-control-button");
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+    locationButton.addEventListener("click", () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+              (position) => {
+                const pos = {
+                  lat: position.coords.latitude,
+                  lng: position.coords.longitude,
+                };
+                    infoWindow.setPosition(pos);
+                    infoWindow.setContent("Location found.");
+                    infoWindow.open(map);
+                    map.setCenter(pos);
+                },
+                () => {
+                    handleLocationError(true, infoWindow, map.getCenter());
+                }
+            );
+        } else{
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
+    });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+        broswerHasGeolocation
+        ? "Error: The Geolocation service failed."
+        : "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(map);
 }
